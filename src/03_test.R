297e2ffbf93937dd5e04b4f2012fc67c0279161b
Keith Hultman <keith.hultman@elmhurst.edu>
2016-12-13 11:18:42 -0600

    removed plyr to test set

--- src/03_test.R
+++ src/03_test.R
@@ -2,7 +2,7 @@
 #
 # 
 #
-suppressMessages(library(plyr))
+
 suppressMessages(library(tidyverse))
 
 test <- read_csv("./data/test.csv")
@@ -15,17 +15,6 @@ test$Summer <- abs(32-test$Week)
 
 test$Date2 <- NULL
 
-test <- test %>% mutate(c.pip = ifelse(Species == "CULEX PIPIENS", 1, 0), 
-                          c.res = ifelse(Species == "CULEX RESTUANS", 1, 0),
-                          c.pip.c.res = ifelse(Species == "CULEX PIPIENS/RESTUANS", 1, 0), 
-                          c.ter = ifelse(Species == "CULEX TERRITANS", 1, 0),
-                          c.oth = ifelse(!(Species %in% c("CULEX PIPIENS", "CULEX RESTUANS", "CULEX PIPIENS/RESTUANS", "CULEX TERRITANS")), 1,0))
-
-test <- test %>% mutate(Species2 = Species) 
-
-test$Species2 <- revalue(test$Species2, c("CULEX ERRATICUS" = "OTHER",
-                                            "CULEX SALINARIUS" = "OTHER",
-                                            "CULEX TERRITANS" = "OTHER"))
 
 test <- select(test, 
                 -Address,
@@ -36,5 +25,19 @@ test <- select(test,
 
 
 save(test, file = "./data/test.RData")
+
+# suppressMessages(library(plyr))
+# test <- test %>% mutate(c.pip = ifelse(Species == "CULEX PIPIENS", 1, 0), 
+#                           c.res = ifelse(Species == "CULEX RESTUANS", 1, 0),
+#                           c.pip.c.res = ifelse(Species == "CULEX PIPIENS/RESTUANS", 1, 0), 
+#                           c.ter = ifelse(Species == "CULEX TERRITANS", 1, 0),
+#                           c.oth = ifelse(!(Species %in% c("CULEX PIPIENS", "CULEX RESTUANS", "CULEX PIPIENS/RESTUANS", "CULEX TERRITANS")), 1,0))
+# 
+# test <- test %>% mutate(Species2 = Species) 
+# 
+# test$Species2 <- revalue(test$Species2, c("CULEX ERRATICUS" = "OTHER",
+#                                             "CULEX SALINARIUS" = "OTHER",
+#                                             "CULEX TERRITANS" = "OTHER"))
+
 
 rm(list = ls())
