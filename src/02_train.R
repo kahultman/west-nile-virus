ec2a13583f42b2bfdcaec8236ca788fa40ac8b6c
kahultman <keith.hultman@elmhurst.edu>
2016-12-15 06:21:36 +0000

    species function

--- src/02_train.R
+++ src/02_train.R
@@ -1,7 +1,8 @@
 # Train
-#suppressMessages(library(plyr))
+
 suppressMessages(library(tidyverse))
 train <- read_csv("./data/train.csv")
+
 
 #train$Date <- as.Date(train$Date, "%Y-%m-%d")
 train$WnvPresent <- as.logical(train$WnvPresent)
@@ -9,6 +10,7 @@ train$WnvPresent <- as.logical(train$WnvPresent)
 train$Date2 <- as.POSIXlt(train$Date)
 train$Year <- train$Date2$year+1900
 train$Week <- floor((train$Date2$yday - train$Date2$wday + 7) /7)
+train$Week <- as.integer(floor((train$Date2$yday - train$Date2$wday + 7) /7))
 train$Summer <- abs(32-train$Week)
 train$Date2 <- NULL
 
@@ -18,11 +20,13 @@ train$Date2 <- NULL
 #                           c.ter = ifelse(Species == "CULEX TERRITANS", 1, 0),
 #                           c.oth = ifelse(!(Species %in% c("CULEX PIPIENS", "CULEX RESTUANS", "CULEX PIPIENS/RESTUANS", "CULEX TERRITANS")), 1,0))
 
+
 add_week_avg_mosq <- function(df){
   weeklyAvgNumMosq <- df %>% group_by(Week) %>% summarise(WeekAvgMos = mean(NumMosquitos)) %>% ungroup()
   df <- left_join(df, weeklyAvgNumMosq, by = "Week")
   df
 }
+
 
 fix_species <- function(x){
   x <- gsub("ERRATICUS", "OTHER", x) %>% 
@@ -48,4 +52,22 @@ save(train, file = "./data/train.RData")
 
 traps <- train %>% group_by(Trap, Longitude, Latitude) %>% summarise(number =n()) %>% arrange(Trap)
 save(traps, file = "./data/traps.RData")
+
+# Species 
+# train <- train %>% mutate(c.pip = ifelse(Species == "CULEX PIPIENS", 1, 0), 
+#                           c.res = ifelse(Species == "CULEX RESTUANS", 1, 0),
+#                           c.pip.c.res = ifelse(Species == "CULEX PIPIENS/RESTUANS", 1, 0), 
+#                           c.ter = ifelse(Species == "CULEX TERRITANS", 1, 0),
+#                           c.oth = ifelse(!(Species %in% c("CULEX PIPIENS", "CULEX RESTUANS", "CULEX PIPIENS/RESTUANS", "CULEX TERRITANS")), 1,0))
+# 
+# train <- train %>% mutate(Species2 = Species) 
+
+
+
+
+
+# train$Species2 <- revalue(train$Species2, c("CULEX ERRATICUS" = "OTHER",
+#"CULEX SALINARIUS" = "OTHER",
+#"CULEX TERRITANS" = "OTHER"))
+
 
