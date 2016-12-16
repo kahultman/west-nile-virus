20a29a8d9690e7136461d13e5139f9d5f516b6f1
kahultman <keith.hultman@elmhurst.edu>
2016-12-16 21:16:45 +0000

    back fill missing rolling average values

--- src/02_train_test.R
+++ src/02_train_test.R
@@ -81,6 +81,17 @@ test <- test %>% group_by(Trap, Year) %>%
          nrow.5d = fnrollsum(nrows, 5),
          nrow.10d = fnrollsum(nrows, 10))
 
+# Back fill the rolling averages with missing data
+
+train$nrow.10d[is.na(train$nrow.10d)] <- train$nrow.5d
+train$nrow.5d[is.na(train$nrow.5d)] <- train$nrow.3d
+train$nrow.3d[is.na(train$nrow.3d)] <- train$nrow
+
+test$nrow.10d[is.na(test$nrow.10d)] <- test$nrow.5d
+test$nrow.5d[is.na(test$nrow.5d)] <- test$nrow.3d
+test$nrow.3d[is.na(test$nrow.3d)] <- test$nrow
+
+
 save(train, file = "./data/train.RData")
 save(test, file = "./data/test.RData")
 
