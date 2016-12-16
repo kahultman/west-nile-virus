39bcc9d4b6fea891d1e5566398c86d9489a43c94
kahultman <keith.hultman@elmhurst.edu>
2016-12-16 02:32:59 +0000

    distance matrix of traps

--- src/06_geo_cluster.R
+++ src/06_geo_cluster.R
@@ -26,3 +26,28 @@ save(train, file = "./data/train.RData")
 test <- left_join(test, trap_geo_hot, by = c("Trap", "Longitude", "Latitude"))
 
 save(test, file = "./data/test.RData")
+
+
+
+# Distance clusters
+
+traps
+
+traps_geo <- as.matrix(traps[,2:3])
+rownames(traps_geo) <- unlist(traps[,1])
+
+
+
+traps.dist <- dist(traps_geo)
+traps.dist
+traps.hclust <- hclust(traps.dist)
+plot(traps.hclust)
+?dist
+
+
+library(sp)
+library(rgeos)
+
+sp.traps <- traps %>% ungroup()
+sp.traps <- sp.traps[unique(sp.traps$Trap)]
+coordinates(sp.traps) <- ~Longitude+Latitude
