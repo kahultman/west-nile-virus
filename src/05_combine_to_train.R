1ec0cce487464ab6efb562c0650714df7f8b7d7c
kahultman <keith.hultman@elmhurst.edu>
2016-12-16 07:36:53 +0000

    move feature engineering off markdown doc

--- src/05_combine_to_train.R
+++ src/05_combine_to_train.R
@@ -29,6 +29,5 @@ save(train, file = "./data/train2.RData")
 load("./data/test.RData")
 test <- left_join(test, station1select, by = "Date")
 save(test, file = "./data/test.RData")
-save(test, file = "./data/test2.RData")
 
 #rm(list= ls()[!(ls() %in% c('test','train'))])
--- src/PrepareSubmission.R
+++ src/PrepareSubmission.R
@@ -8,9 +8,15 @@ sample
 
 glm.submission <- select(test, Id, WnvPresent = Wnv.glm.Pred)
 sum(is.na(glm.submission$WnvPresent))
-
 write_csv(glm.submission, path = "./data/glm.submission.csv")
+
+
+glmboos.submission <- select(test, Id, WnvPresent = glmboost.Pred2)
+sum(is.na(glmboos.submission$WnvPresent))
+write_csv(glmboos.submission, path = "./data/glmboos.submission.csv")
+
 
 ggplot(data = train, aes(sample = NumMosquitos)) + 
   stat_qq(distribution = stats::qpois, dparams = list(lambda = mean(train$NumMosquitos))) 
 
+save(test, file = "./data/test.RData")
